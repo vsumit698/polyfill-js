0084dba60e77146d329a1504a143ac0a164a9249
vsumit698 <50446660+vsumit698@users.noreply.github.com>
2022-02-10 15:14:14 +0530

    Added implementation of call function

---
+++ FunctionPolyfills/call.js
@@ -0,0 +1,43 @@
+/* Problem Statement
+
+  1. Implemented call polyfill using apply method 
+*/
+
+// Code
+Function.prototype.myCall = function(thisObj,...restParams){
+  this.apply(thisObj,restParams);
+}
+/* Problem Statement
+
+  1. Implemented call polyfill without using apply method 
+*/
+
+/* Thought Process
+  
+  1. Requirement is we need to invoke function on which call method is invoked, such that its this keyword points to thisArg reference.
+  2. To make this happen we need to invoke that function from thisArg object beacuse this keyword reference depends on while we invoke function (only including regular function here).
+  3. To do that we need to attach that function object to thisArg object, and invoke that function from thisArg object. Make sure thisArg should always be of type object. If it's primitive value, its need to transform to object.
+  4. To attach function object in thisArg object, we need key where function object(on which call method is invoked) as value. For proper key so that it will not conflict with existing keys of thisArg object.
+  5. Now here need of Symbol datatype arises, where these symbol keys are also not enumerable i.e these keys, keys values are not available at for in and for of loop and also acts as a unique key in object.
+  6. After invoking function from thisArg object, delete inserted key from thisArg object, so after completing operation thisArg comes to original state.
+*/
+
+// Code
+Function.prototype.myCall = function(thisArg, ...args) {
+  thisArg = thisArg===null || thisArg===undefined ? window : thisArg;
+  thisArg = Object(thisArg);
+  let symKey = Symbol();
+  thisArg[symKey] = this;
+  let result = thisArg[symKey](...args);
+  delete thisArg[symKey];
+  return result;
+}
+
+// testing functionality
+let obj = {name:"sumit"};
+
+function fun(a,b,c){
+  console.log(this===obj,a,b,c);
+}
+
+fun.myCall(obj,1,2,3);
---
+++ readme.md
@@ -0,0 +1,46 @@
+# Polyfills/JavaScript implementation  List -
+
+## Function Polyfills - 
+1. Implement Function.bind using call/apply 
+1. Implement Function.apply using call 
+1. Implement Function.call using apply **(DONE)**
+
+## Array Polyfills - 
+1. Array.map method on Array prototype 
+1. Array.filter method on the Array Prototype 
+1. Array.find method on the Array Prototype 
+1. Array.forEach method on the Array Prototype 
+1. Array.reduce method on the Array Prototype 
+1. Array.flat method (that will flatten an array into a 1D array) Array Prototype 
+
+## Promise Polyfills / Async Helpers
+1. Implement Async.parallel, which executes a set of async tasks parallelly
+1. Implement Async.series, which executes a set of async tasks in series
+1. Write a method that will implement Promise.all 
+1. Write a method that will implement Promise.race
+1. Write a method that will implement Promise.finally
+1. Write a method that will implement Promise.any
+1. Implement Promise class
+
+## Functional Programming
+1. implement curry()
+1. Implement the pipe() polyfill
+1. Implement the compose() polyfill
+## JS Problems - 
+1. Implement SetInterval Polyfill using setTimeout 
+1. Implement debounce 
+1. Implement throttle 
+1. Write a function memoize(memo) that will subsequent calls to a function 
+1. Inheritance in JS using Constructor functions
+1. Map class
+1. Set class
+## Design Patterns
+1. Attach a listener to an array which gets triggered when an item is pushed into the array
+1. Implement a class/function/utility which exhibits an event dispatcher mechanism.
+1. Implement an Event Emitter class.
+1. Implement the publisher-subscriber(pub-sub) pattern in javascript
+
+
+## Refer sites
+  * JS Vault
+  * javascriptinterviewquestions
